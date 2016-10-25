0ecdecbcdd2533a50b9c9a82099223cd409f1b74
shamshudein <ShamshudeinSyed.Kareem@cognizant.com>
2016-10-25 15:01:23 +0530

    Angular Component Router

---
+++ componentrouter/movie-app-component.js
@@ -0,0 +1,26 @@
+(function () {
+    "use strict";
+
+    var module = angular.module("movieListApp");
+
+    module.component("movieApp",{
+        templateUrl: "movie-app-component.html",
+        $routeConfig: [
+            {
+                path: "/list",
+                component: "movieList",
+                name: "List"
+            },
+            {
+                path: "/about",
+                component: "appAbout",
+                name: "About"
+            },
+            {
+                path: "/**",
+                redirectTo: ["List"]
+            }
+        ]
+    });
+
+} ());
